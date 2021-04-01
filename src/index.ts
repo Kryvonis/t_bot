b430904efea9029c50eb84f953ee7bb3c165f4c9
kryvonis <kryvonis.artem@gmail.com>
2021-04-02 00:27:34 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 /* eslint-disable prefer-arrow-callback */
-import { config } from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
-
-const dev = process.env.NODE_ENV !== 'production';
 
 /**
  * This example demonstrates setting up webhook
@@ -11,10 +8,10 @@ const dev = process.env.NODE_ENV !== 'production';
 const TOKEN = process.env.TELEGRAM_TOKEN || '551431067:AAHcuQ2u0RFykDYgHYRz9huelryCsHTx9to';
 const options = {
   webHook: {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
   },
 };
-const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
+const url = process.env.APP_URL || 'https://tbotpiddor.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, options as any);
 
 // This informs the Telegram servers of the new webhook.
@@ -22,6 +19,6 @@ const bot = new TelegramBot(TOKEN, options as any);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 // Just to ping!
-bot.on('message', function onMessage(msg) {
+bot.on('message', function onMessage(msg:any) {
   bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
